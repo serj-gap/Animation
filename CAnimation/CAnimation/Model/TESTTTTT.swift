de560423744b031c238de728faf0c3b4febd4efe
sergey_titov <sierj.titov.001@gmail.com>
2020-09-02 22:31:25 +0300

    COmmit

---
+++ CAnimation/CAnimation/Model/TESTTTTT.swift
@@ -0,0 +1,14 @@
+//
+//  TESTTTTT.swift
+//  CAnimation
+//
+//  Created by Sergey Titov on 9/2/20.
+//  Copyright © 2020 Sergey Titov. All rights reserved.
+//
+
+import Foundation
+
+class TestGit {
+    
+    var nuber = 5
+}
